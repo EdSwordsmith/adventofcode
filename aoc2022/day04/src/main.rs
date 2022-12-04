10e0d1c40b868164f04f60a082c3ae69cce0bb68
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-12-04 09:55:27 +0000

    Day 4 - 2022 (in rust :D)

---
+++ aoc2022/day04/src/main.rs
@@ -0,0 +1,53 @@
+use std::io;
+
+struct SectionRange {
+    from: u32,
+    to: u32,
+}
+
+impl SectionRange {
+    fn from_str(s: &str) -> SectionRange {
+        let (from, to) = s.split_once('-').unwrap();
+        let from = from.parse().unwrap();
+        let to = to.parse().unwrap();
+        SectionRange { from, to }
+    }
+
+    fn contains(&self, other: &SectionRange) -> bool {
+        other.from >= self.from && other.to <= self.to
+    }
+
+    fn overlaps(&self, other: &SectionRange) -> bool {
+        (other.from >= self.from && other.from <= self.to)
+            || (other.to <= self.to && other.to >= self.from)
+    }
+}
+
+fn parse_line(line: String) -> (SectionRange, SectionRange) {
+    let (first_range, second_range) = line.trim().split_once(',').unwrap();
+    (
+        SectionRange::from_str(first_range),
+        SectionRange::from_str(second_range),
+    )
+}
+
+fn main() {
+    let pairs: Vec<(SectionRange, SectionRange)> = io::stdin()
+        .lines()
+        .map(|line| parse_line(line.unwrap()))
+        .collect();
+
+    let first_part = pairs
+        .iter()
+        .filter(|(r1, r2)| r1.contains(r2) || r2.contains(r1))
+        .count();
+
+    println!("Part 1: {}", first_part);
+
+    let second_part = pairs
+        .iter()
+        .filter(|(r1, r2)| r1.overlaps(r2) || r2.overlaps())
+        .count();
+
+    println!("Part 1: {}", second_part);
+}
