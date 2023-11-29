b8bc35634138d6bdedb9a6780efeb2f4ee74aaa2
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-11-29 22:14:43 +0000

    add flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust
+            cargo
+            rustc
+            rustfmt
+            clippy
+            # C
+            gnumake
+            clang
+            # Python
+            python3
+          ];
+        };
+      });
+}
